fa039185be16be464c0438e2322c22fabaa4d315
chip6001@yandex.ru <alexander@NBLK-WAX9X-b55e7d90.localdomain>
2023-11-24 13:33:35 +0300

    2

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -2,7 +2,6 @@ package com.example.plugins
 
 import com.example.routes.userRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
