9bd130cc52366b49e720ed08fdf00d6dbd59409f
chip6001@yandex.ru <alexander@NBLK-WAX9X-b55e7d90.localdomain>
2023-11-24 13:32:20 +0300

    2

--- src/main/kotlin/com/example/routes/UserRoutes.kt
+++ src/main/kotlin/com/example/routes/UserRoutes.kt
@@ -1,2 +1,44 @@
 package com.example.routes
 
+import com.example.models.*
+import com.example.security.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.userRouting(){
+    route("/user") {
+        get{
+            if (userStorage.isNotEmpty()){
+                call.respondText(
+                    "Users found",
+                    status = HttpStatusCode.OK
+                )
+            }else{
+                call.respondText("Users not found",
+                    status = HttpStatusCode.OK
+                )
+            }
+        }
+
+        get("{id?}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+            val user = userStorage.find {it.id == id} ?: return@get call.respondText(
+                "No users with id $id",
+                status = HttpStatusCode.NotFound
+            )
+            user.hash = Hashing.getHash(user.pass.toByteArray(), user.coder)
+            call.respondText("User with id - $id have password hash - ${user.hash}!")
+        }
+        post{
+            val user = call.receive<User>()
+            userStorage.add(user)
+            call.respondText("User stored correctly", status = HttpStatusCode.Created)
+        }
+    }
+}
--- src/main/kotlin/com/example/security/Hashing.kt
+++ src/main/kotlin/com/example/security/Hashing.kt
@@ -1,4 +1,16 @@
 package com.example.security
 
-class Hashing {
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+object Hashing {
+    //MD2, MD5, SHA-2, SHA-224, SHA-256, SHA-384, SHA-512
+    @Throws(NoSuchAlgorithmException::class)
+    fun getHash(inByte: ByteArray, type: String) : String{
+        val digestedBytes = MessageDigest.getInstance(type).digest(inByte)
+        return with(StringBuilder()){
+            digestedBytes.forEach { b -> append(String.format("%02X", b)) }
+            toString().lowercase()
+        }
+    }
 }
